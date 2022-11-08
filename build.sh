5ad21ac771bade4261f5279cec6569ba00ec1901
Ben <benjamin.etheredge@gmail.com>
2022-11-08 05:38:25 +0000

    Adding 32 bit arm

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # docker buildx build --platform linux/amd64 -t etheredgeb/gloves2 --push .
-docker buildx build --platform linux/amd64,linux/arm64 -t etheredgeb/gloves2 --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t etheredgeb/gloves2:$(date +%s) --push .
